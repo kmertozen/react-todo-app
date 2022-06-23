a063e9808d8c26076b376a703243d59fec855309
Mert <raltahook07@gmail.com>
2022-06-23 17:08:28 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 import Provider from "./context"
@@ -9,10 +9,8 @@ function App() {
   return (
 
     <Provider>
-
       <Form />
       <TodoList />
-
     </Provider>
 
 
