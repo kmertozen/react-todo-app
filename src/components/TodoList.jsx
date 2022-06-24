ee087c7e9312372bd9257155d31276fe10cee7ea
Mert <raltahook07@gmail.com>
2022-06-25 00:46:59 +0300

    todolistcontext

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
-import Todo from './Todo'
 import { useSite } from "../context"
-
 function TodoList() {
-    
-  const {todos,setTodos,filteredTodos} = useSite();
+
+    const { todos, setTodos, filteredTodos } = useSite();
+    const deleteHandler = (id) => {
+        setTodos(todos.filter((tds) => tds.id !== id))
+    }
+    const complateHandler = (id) => {
+
+        setTodos(todos.map((todoitem) => {
+            if (todoitem.id === id) {
+                return {
+                    ...todoitem, completed: !todoitem.completed
+                }
+            }
+            return todoitem;
+        }))
+
+    }
     return (
 
 
         <div className="todo-container">
             <ul className="w-50 p-0 rounded-bottom">
-                {filteredTodos.map((todo)=>(
-                    <Todo
-                    text={todo.text}
-                    todo={todo}
-                    key={todo.id}
-                    todos={todos}
-                    setTodos={setTodos}
-                    />
+                {filteredTodos.map((todo) => (
+                    <li className={`todo ${todo.completed ? "completed" : ""} list-group-item d-flex  rounded-0`}>
+
+                        <span className="todo-item mr-auto">{todo.text}</span>
+                        <button className="btn btn-success mr-2" onClick={() => complateHandler(todo.id)}>
+                            Tamamla
+                        </button>
+                        <button className="btn btn-danger" onClick={() => deleteHandler(todo.id)}>
+                            Kaldır
+                        </button>
+
+                    </li>
                 ))}
-                {todos.length === 0 && (
-				<li className='list-group-item text-center'>Henüz bir todo eklemediniz!</li>
-			)}
-                
+                {filteredTodos.length === 0 &&(<li className='list-group-item text-center'>Todo Bulunamadı</li>)}
+
             </ul>
         </div>
 
